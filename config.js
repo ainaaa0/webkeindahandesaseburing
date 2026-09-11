4360ff5d3441c7aab16c3b47cbf99a353707701c
ainaaa0 <ainaamelaa@gmail.com>
2026-09-11 02:35:25 -0700

    Update API_URL to new endpoint

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbyrg4EYrO_BBuDVQPthJ1pXJ4f_uKAjD2i2uc9Mwznf4etyM9JlmwSrpnAzEkgh1fs/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbwns5XmcrEC_X8N29_Vw9dhTI4Rbt5StGG2P2nMD1ED5sYJB2Dc-n1bSCql5nGf284p/exec";
