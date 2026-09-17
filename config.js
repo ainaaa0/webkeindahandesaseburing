9d1a61c7b2b5921e8b40612520741fafe27a8039
ainaaa0 <ainaamelaa@gmail.com>
2026-09-17 16:25:58 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbyhLKnyoh8OBmnQxPddegMzyPvrcowZmGcrlFu7PwSzgeGWC6vYet4woufetKrtK_Jg/exec";
+const API_URL =" https://script.google.com/macros/s/AKfycbyhLKnyoh8OBmnQxPddegMzyPvrcowZmGcrlFu7PwSzgeGWC6vYet4woufetKrtK_Jg/exec";
